ac0eff30687b3163283f3c1be6b4b2326fa931a3
keshkesh-down <58878384+keshkesh-down@users.noreply.github.com>
2022-12-05 22:47:16 +0300

    Git Init

---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+	internal class TodoModel: INotifyPropertyChanged
+	{
+		private bool isDone;
+		private string text;
+
+		public DateTime CreationDate { get; set; } = DateTime.Now;
+
+		public bool IsDone
+		{
+			get { return isDone; }
+			set
+			{
+				if (IsDone == value)
+						return;
+				isDone = value;
+				OnPropertyChanged("IsDone");
+			}
+		}
+
+		public string Text
+		{
+			get { return text; }
+			set
+			{
+				if (text == value)
+					return;
+				text = value;
+				OnPropertyChanged("Text");
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke(propertyName, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
